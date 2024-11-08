64294d9c589c2ca6eb3c72983fa95a05294bb322
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2024-11-08 20:03:09 +0100

    feat: add redis support

--- apps/backend/src/kv.ts
+++ apps/backend/src/kv.ts
@@ -1,6 +1,11 @@
 import { createStorage } from "unstorage";
 import fsDriver from "unstorage/drivers/fs";
+import redisDriver from "unstorage/drivers/redis";
 
 export const kv = createStorage({
-  driver: fsDriver({ base: "./kv" }),
+  driver: process.env.NODE_ENV === 'development' ? fsDriver({ base: "./kv" }) : redisDriver({
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT!),
+    password: process.env.REDIS_PASSWORD,
+  }),
 });
